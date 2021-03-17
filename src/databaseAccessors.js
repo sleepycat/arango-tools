48b1755d6fc5c7f97d840ce30399c14dad57c77b
Mike Williamson <mike@korora.ca>
2021-03-17 15:08:38 -0400

    Remove proxies
    This commit removes the proxy objects that papered over Arangojs's
    removal of forEach and txn.run.

--- src/databaseAccessors.js
+++ src/databaseAccessors.js
@@ -3,17 +3,8 @@ const { aql } = require('arangojs')
 function databaseAccessors({ connection, rootConnection }) {
   return {
     query: async function query(strings, ...vars) {
-      const cursor = await connection.query(aql(strings, ...vars), {
+      return connection.query(aql(strings, ...vars), {
         count: true,
-      })
-      return new Proxy(cursor, {
-        get(target, name, receiver) {
-          // arangojs renamed each to forEach
-          if (name === 'each') {
-            return Reflect.get(target, 'forEach', receiver)
-          }
-          return Reflect.get(target, name, receiver)
-        },
       })
     },
     drop: () => {
@@ -33,16 +24,7 @@ function databaseAccessors({ connection, rootConnection }) {
       return true
     },
     transaction: async function transaction(collections) {
-      const cursor = await connection.beginTransaction(collections)
-      return new Proxy(cursor, {
-        get(target, name, receiver) {
-          // arangojs renamed run to step
-          if (name === 'run') {
-            return Reflect.get(target, 'step', receiver)
-          }
-          return Reflect.get(target, name, receiver)
-        },
-      })
+      return connection.beginTransaction(collections)
     },
   }
 }
